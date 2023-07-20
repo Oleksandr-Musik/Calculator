d78d38c295a2cba1637ce6d9862fd945b426e42c
Oleksandr <Oleksandr_Musik@gmail.com>
2023-07-20 16:27:53 +0300

    I created and added methods

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static int sub(int a, int b) {
+        return a - b;
+    }
+    static int mul(int a, int b) {
+        return a * b;
+    }
+    static float div(float a, float b) {
+        if (b==0)
+            System.out.println("На нуль ділити не можна!");
+        return a/b;
+    }
     public static void main(String[] args) {
 
     }
